486d623af481ee70a3b2a78aebb168394a3cc1e7
Nikita <nkt@rambler.ru>
2022-09-14 19:04:18 +0300

    1

--- src/main/java/ru/kata/spring/boot_security/demo/dao/UserDaoImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDaoImpl.java
@@ -1,17 +1,15 @@
 package ru.kata.spring.boot_security.demo.dao;
 
 import org.springframework.stereotype.Repository;
-import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
-import java.util.Set;
 
 @Repository
 public class UserDaoImpl implements UserDao {
-    List<User> list;
+
     @PersistenceContext
     private EntityManager entityManager;
 
@@ -23,38 +21,26 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public User findById(int id) {
-        list = entityManager.createQuery("select user from User user where user.id =: idParam", User.class)
-                .setParameter("idParam", id).getResultList();
-        if (list != null) {
-            return list.get(0);
-        } else {
-            return null;
-        }
+        return entityManager.createQuery("select user from User user where user.id =: idParam", User.class)
+                .setParameter("idParam", id).getSingleResult();
     }
 
     @Override
     public void saveUser(User user) {
-         entityManager.merge(user);
+        entityManager.merge(user);
     }
 
     @Override
     public void deleteById(int id) {
         User userToBeDeleted = findById(id);
         entityManager.remove(userToBeDeleted);
-
     }
 
     @Override
     public User findByUsername(String userName) {
-        list = entityManager.createQuery("select user from User user where user.username =: usernameParam", User.class)
-                .setParameter("usernameParam", userName).getResultList();
-        if (list != null) {
-            return list.get(0);
-        } else {
-            return null;
-        }
-
-
+        return entityManager.createQuery("select user from User user where user.username =: usernameParam", User.class)
+                .setParameter("usernameParam", userName).getSingleResult();
     }
-
 }
+
+
